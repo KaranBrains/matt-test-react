0ecb3a2a621c8ccba805116bdc9664edc79333c1
Karan Bains <karanbains.bains@gmail.com>
2021-08-06 19:56:25 +0530

    search funtionality completed

--- social-media/src/components/posts/index.tsx
+++ social-media/src/components/posts/index.tsx
@@ -6,13 +6,24 @@ import { Dispatch } from "redux"
 import { IPost } from "../../types/post"
 import { RootReducer } from "../../redux/reducers/rootReducer"
 import { Post } from "./postCard"
+import "./styles.css"
 
 const App: React.FC = () => {
 
-  const posts: readonly IPost[] = useSelector(
+  const [filterText, setFilterText] = React.useState<string>("");
+
+  const posts:IPost[] = useSelector(
     (state: RootReducer) => state?.postsReducer.posts,
     shallowEqual
   )
+
+  const filteredItems = posts.filter(
+    (post:IPost) =>
+      post.body.toLocaleLowerCase().includes(filterText) ||
+      post.title.toLocaleLowerCase().includes(filterText)
+  );
+
+  const itemsToDisplay: IPost[] = filterText ? filteredItems : posts;
 
   const dispatch: Dispatch<any> = useDispatch()
 
@@ -24,9 +35,16 @@ const App: React.FC = () => {
   return (
     <div className="container">
       <div className="col-lg-8 col-md-9 col-sm-10 col-10 mx-auto my-5">
-        <h1>All Posts</h1>
-        {posts && posts.length > 0 ? (
-          posts.map((p: IPost) => {
+        <div className="row">
+          <h1 className="col-lg-8 col-md-8 col-sm-6 col-6">All Posts</h1>
+          <input 
+            className="col-lg-4 col-md-4 col-sm-6 col-6 search-textbox my-auto" 
+            type="text" 
+            placeholder="Search"
+            onChange={(event)=>setFilterText(event.target.value)} />
+        </div>
+        {itemsToDisplay && itemsToDisplay.length > 0 ? (
+          itemsToDisplay.map((p: IPost) => {
             return (
               <Post post={p} key={p.id} />
             )
